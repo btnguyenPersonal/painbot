8b88b17b16febb4ea61c91839827edaf7d5763e3
btnguyenPersonal <peteraxefan@gmail.com>
2025-04-27 20:59:16 -0500

    add TODOs and helper functions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # painbot
+
+### TODO:
+hook up two 9v
+set up pins on pi to two controllers
+after it works proof of concept, solder split wires together
+build battery holder??
+make helper functions to go forward, back turn left & right
+push updates to discord server for logging/interaction
+random cat-like movements?? will have to test how this is going to work
+speaker
+mic
+camera
+secure wheels better
+flask server to update repo on github action?? does cost money??
+local website??
--- painbot.py
+++ painbot.py
@@ -6,6 +6,29 @@ class Vehicle:
         self.left_motor = Motor(forward=17, backward=22)
         self.left_pwm = PWMOutputDevice(18)
         self.left_pwm.value = 0.5
+        self.right_motor = Motor(forward=23, backward=24)
+        self.right_pwm = PWMOutputDevice(25)
+        self.right_pwm.value = 0.5
+
+    def stop(self):
+        rpi_vehicle.left_motor.stop()
+        rpi_vehicle.right_motor.stop()
+
+    def forward(self):
+        rpi_vehicle.left_motor.forward()
+        rpi_vehicle.right_motor.forward()
+
+    def backward(self):
+        rpi_vehicle.left_motor.backward()
+        rpi_vehicle.right_motor.backward()
+
+    def turn_left(self):
+        rpi_vehicle.left_motor.backward()
+        rpi_vehicle.right_motor.forward()
+
+    def turn_right(self):
+        rpi_vehicle.left_motor.forward()
+        rpi_vehicle.right_motor.backward()
 
 rpi_vehicle = Vehicle()
 
